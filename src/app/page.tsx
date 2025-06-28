bf17d48ec3c3a6dadee054a6e020685eaea156c5
Mena Maged <menaxmaged@gmail.com>
2025-06-28 22:45:33 +0300

    >>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 // app/page.js
 import HeroSection from '@/components/Hero';
 import AboutUs from '@/components/AboutUs';
-import Sponsors from '@/components/Sponsors';
+// import Sponsors from '@/components/Sponsors';
 import Highlights from '@/components/Highlights';
-import Speakers from '@/components/Speakers';
-import Agenda from '@/components/Agenda';
-import Testimonials from '@/components/Testimonials';
+// import Speakers from '@/components/Speakers';
+// import Agenda from '@/components/Agenda';
+// import Testimonials from '@/components/Testimonials';
 import Registration from '@/components/Registration';
 import Pricing from '@/components/Pricing';
-import Faq from '@/components/Faq';
+// import Faq from '@/components/Faq';
 
 export default function HomePage() {
   return (
